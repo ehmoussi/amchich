a252fe7c3dac5e31f3825b45dddbf18bcc8220f8
El Hadi Moussi <50993404+ehmoussi@users.noreply.github.com>
2025-05-25 12:08:45 +0200

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@
 function App() {
 
     return (
-        <>
-        </>
+        <main className="ring-none mx-auto flex h-svh max-h-svh w-full max-w-[80rem] flex-col items-stretch border-none">
+            Hello World
+        </main >
     )
 }
 
