311fa3e6493bbc21dd29926e51a8dfea03f7a435
El Hadi Moussi <50993404+ehmoussi@users.noreply.github.com>
2025-06-09 19:11:55 +0200

    Remove openai for retrieving models names

--- src/lib/llmmodels.ts
+++ src/lib/llmmodels.ts
@@ -1,4 +1,3 @@
-import OpenAI from "openai";
 import { createModel, setModels, type LLMID, type LLMModel } from "./db";
 
 
@@ -20,14 +19,14 @@ export async function updateAvailableModels(signal: AbortSignal) {
 }
 
 async function fetchOllamaModels(signal: AbortSignal): Promise<LLMID[]> {
-    const client = new OpenAI({
-        baseURL: "http://localhost:11434/v1/",
-        apiKey: "ollama",
-        dangerouslyAllowBrowser: true,
-    });
     const names: LLMID[] = [];
-    for await (const model of await client.models.list({ signal: signal })) {
-        names.push(model.id);
+    const response = await fetch("http://localhost:11434/api/tags", {
+        method: "GET",
+        signal
+    });
+    const data = await response.json();
+    for (const model of data.models) {
+        names.push(model.name);
     }
     return names;
 }
@@ -35,55 +34,46 @@ async function fetchOllamaModels(signal: AbortSignal): Promise<LLMID[]> {
 
 
 async function fetchOpenAiModels(signal: AbortSignal): Promise<LLMID[]> {
-    const client = new OpenAI({
-        baseURL: "http://localhost:3001/api/v1/openai",
-        apiKey: "dummy",
-        dangerouslyAllowBrowser: true,
-    });
     const names: LLMID[] = [];
-    const models = await client.models.list({ signal: signal });
-    for await (const model of models) {
-        if (model.owned_by === "system")
-            names.push(model.id);
-    }
-    return names;
-}
-
-
-async function fetchOpenRouterModels(signal: AbortSignal): Promise<LLMID[]> {
-    const client = new OpenAI({
-        baseURL: "http://localhost:3001/api/v1/openrouter",
-        apiKey: "dummy",
-        dangerouslyAllowBrowser: true,
-    });
-    const names: LLMID[] = [];
-    const models = await client.models.list({ signal: signal });
-    for await (const model of models) {
+    const response = await fetch(
+        "http://localhost:3001/api/v1/openai/models",
+        { method: "GET", signal }
+    );
+    const data = await response.json();
+    for (const model of data.data) {
         names.push(model.id);
     }
     return names;
 }
 
 
+async function fetchOpenRouterModels(signal: AbortSignal): Promise<LLMID[]> {
+    const names: LLMID[] = [];
+    const response = await fetch(
+        "http://localhost:3001/api/v1/openrouter/models",
+        { method: "GET", signal }
+    );
+    const data = await response.json();
+    for (const model of data.data) {
+        if (model.architecture.output_modalities.includes("text")) {
+            names.push(model.id);
+        }
+    }
+    return names;
+}
+
+
 export async function getOpenAIExpense(): Promise<number> {
-    const client = new OpenAI({
-        baseURL: "http://localhost:3001/api/v1/openai",
-        apiKey: "dummy",
-        dangerouslyAllowBrowser: true,
-    });
     const now = new Date();
     const firstDayOfTheMonth = new Date(now.getFullYear(), now.getMonth(), 1);
     const startTime = Math.floor(firstDayOfTheMonth.getTime() / 1000);
-    const response = await client.request({
-        method: "get",
-        path: "/organization/costs",
-        query: {
-            start_time: String(startTime),
-            limit: "100",
-        },
-    }) as any;
+    const response = await fetch(
+        `http://localhost:3001/api/v1/openai/organization/costs?start_time=${String(startTime)}&limit=${100}`,
+        { method: "GET", }
+    );
+    const data = await response.json();
     let totalSpent = 0;
-    for (const bucket of response.data) {
+    for (const bucket of data.data) {
         for (const item of bucket.results) {
             totalSpent += item.amount.value;
         }
